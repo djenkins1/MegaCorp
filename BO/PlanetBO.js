e59a5c207e45f8ac1213d592a95bd4c63d095858
djenkins1 <djenkins1@email.arizona.edu>
2018-08-31 20:41:04 -0700

    Added planet business module and tests

---
+++ BO/PlanetBO.js
@@ -0,0 +1,58 @@
+const planetDAO = require( "../Data/PlanetDAO" );
+const DEBUG_MODE = require('config').get('DebugModeBO');
+const ID_KEY = require('config').get('ID_KEY');
+const MAX_TAX_RATE = require('config').get('MAX_TAX_RATE');
+
+/*
+ChangeName,changes the name of the planet(NO UPDATE DB YET)
+AddMoney,Adds money to the planet(NO UPDATE DB YET)
+RemoveMoney,Removes money from the planet(NO UPDATE DB YET)
+AddPopulation,Adds population to the planet(NO UPDATE DB YET)
+RemovePopulation,Removes population from the planet(NO UPDATE DB YET)
+
+DB:
+GetDockedShips,Returns all ships that are docked on planet(Have Z in location that matches planet's Z)
+GetAllBuildings,Returns all buildings that are on the planet
+GetCompaniesByHQ,Returns all companies that have headquarters on planet
+SavePlanet,Saves any changes to the planet to the database
+AllPlanets,Returns all planets in the database
+CreatePlanet,Creates a new planet and adds it to the database
+DefaultObj,Returns the default prototype for a new object
+*/
+
+//ChangeTaxes,changes the tax rate of the planet(NO UPDATE DB YET)
+//returns the new tax rate if updated or undefined otherwise
+function changeTaxes( obj, newRate )
+{
+    DEBUG_MODE && console.log( "Calling changeTaxes in PlanetBO, new rate:" , newRate );
+    if ( obj == undefined )
+    {
+        DEBUG_MODE && console.log( "PlanetBO.changeTaxes: obj is undefined" );
+        return undefined;
+    }
+
+    if ( newRate == undefined )
+    {
+        DEBUG_MODE && console.log( "PlanetBO.changeTaxes: newRate is undefined" );
+        return undefined;
+    }
+
+    if ( obj.taxes == undefined )
+    {
+        DEBUG_MODE && console.log( "PlanetBO.changeTaxes: obj.taxes is undefined" );
+        return undefined;
+    }
+
+    let newRateParsed = parseFloat( newRate );
+    if ( newRateParsed < 0 || newRateParsed > MAX_TAX_RATE )
+    {
+        DEBUG_MODE && console.log( "PlanetBO.changeTaxes: newRate not in valid range" );
+        return undefined;
+    }
+
+    obj.taxes = newRateParsed;
+    DEBUG_MODE && console.log( "PlanetBO.changeTaxes: changed taxes successfully" );
+    return newRateParsed;
+}
+
+exports.changeTaxes = changeTaxes;
---
+++ tests/testPlanetBO.js
@@ -0,0 +1,192 @@
+const assert = require('assert');
+const dataAPI = require( '../Data/DataAPI' );
+const bo = require( '../BO/PlanetBO' );
+const MAX_TAX_RATE = require('config').get('MAX_TAX_RATE');
+
+//This test file does not test database functionality
+describe('TestPlanetBO', function()
+{
+    //The before() callback gets run before all tests in the suite.
+    before( function( done )
+    {
+        done();
+    });
+
+    //The beforeEach() callback gets run before each test in the suite.
+    beforeEach( function( done )
+    {
+        done();
+    });
+
+    /*
+    ===========
+    changeTaxes testing begins
+    ===========
+    */
+    it( 'test changeTaxes success normal rate' , function( done )
+    {
+        let mockDataList = JSON.parse(JSON.stringify( require( "../sampleData/testData/planetsDifferentSystems.json" ) ) );
+        assert.ok( mockDataList );
+        assert.ok( mockDataList.length );
+        assert.ok( mockDataList[ 0 ] );
+        assert.ok( mockDataList[ 0 ].taxes );
+
+        let testObj = mockDataList[ 0 ];
+        let testRate = 0.11;
+        assert.ok( testRate <= MAX_TAX_RATE );
+
+        let result = bo.changeTaxes( testObj , testRate );
+        assert.ok( result );
+        assert.equal( result, testRate );
+        assert.equal( testObj.taxes, testRate );
+        done();
+    });
+
+    it( 'test changeTaxes success for zero tax rate' , function( done )
+    {
+        let mockDataList = JSON.parse(JSON.stringify( require( "../sampleData/testData/planetsDifferentSystems.json" ) ) );
+        assert.ok( mockDataList );
+        assert.ok( mockDataList.length );
+        assert.ok( mockDataList[ 0 ] );
+        assert.ok( mockDataList[ 0 ].taxes );
+
+        let testObj = mockDataList[ 0 ];
+        let testRate = 0.0;
+        assert.ok( testRate <= MAX_TAX_RATE );
+
+        let result = bo.changeTaxes( testObj , testRate );
+        assert.equal( result, testRate );
+        assert.equal( testObj.taxes, testRate );
+        done();
+    });
+
+    it( 'test changeTaxes success for max tax rate' , function( done )
+    {
+        let mockDataList = JSON.parse(JSON.stringify( require( "../sampleData/testData/planetsDifferentSystems.json" ) ) );
+        assert.ok( mockDataList );
+        assert.ok( mockDataList.length );
+        assert.ok( mockDataList[ 0 ] );
+        assert.ok( mockDataList[ 0 ].taxes );
+
+        let testObj = mockDataList[ 0 ];
+        let testRate = MAX_TAX_RATE;
+
+        let result = bo.changeTaxes( testObj , testRate );
+        assert.ok( result );
+        assert.equal( result, testRate );
+        assert.equal( testObj.taxes, testRate );
+        done();
+    });
+
+    it( 'test changeTaxes failure for too high tax rate' , function( done )
+    {
+        let mockDataList = JSON.parse(JSON.stringify( require( "../sampleData/testData/planetsDifferentSystems.json" ) ) );
+        assert.ok( mockDataList );
+        assert.ok( mockDataList.length );
+        assert.ok( mockDataList[ 0 ] );
+        assert.ok( mockDataList[ 0 ].taxes );
+
+        let testObj = mockDataList[ 0 ];
+        let testRate = MAX_TAX_RATE + 0.1;
+        let oldRate = mockDataList[ 0 ].taxes;
+
+        let result = bo.changeTaxes( testObj , testRate );
+        assert.equal( testObj.taxes, oldRate );
+        if ( result )
+        {
+            assert.fail( "changeTaxes returned for too high tax rate: " + result );
+        }
+
+        done();
+    });
+
+    it( 'test changeTaxes failure for negative tax rate' , function( done )
+    {
+        let mockDataList = JSON.parse(JSON.stringify( require( "../sampleData/testData/planetsDifferentSystems.json" ) ) );
+        assert.ok( mockDataList );
+        assert.ok( mockDataList.length );
+        assert.ok( mockDataList[ 0 ] );
+        assert.ok( mockDataList[ 0 ].taxes );
+
+        let testObj = mockDataList[ 0 ];
+        let testRate = -0.1;
+        let oldRate = mockDataList[ 0 ].taxes;
+
+        let result = bo.changeTaxes( testObj , testRate );
+        assert.equal( testObj.taxes, oldRate );
+        if ( result )
+        {
+            assert.fail( "changeTaxes returned for negative tax rate: " + result );
+        }
+
+        done();
+    });
+
+    it( 'test changeTaxes failure for undefined obj' , function( done )
+    {
+        let testObj = undefined
+        let testRate = 0.1;
+        assert.ok( testRate <= MAX_TAX_RATE );
+
+        let result = bo.changeTaxes( testObj , testRate );
+        if ( result )
+        {
+            assert.fail( "changeTaxes returned for undefined obj: " + result );
+        }
+
+        done();
+    });
+
+    it( 'test changeTaxes failure for undefined newRate' , function( done )
+    {
+        let mockDataList = JSON.parse(JSON.stringify( require( "../sampleData/testData/planetsDifferentSystems.json" ) ) );
+        assert.ok( mockDataList );
+        assert.ok( mockDataList.length );
+        assert.ok( mockDataList[ 0 ] );
+        assert.ok( mockDataList[ 0 ].taxes );
+
+        let testObj = mockDataList[ 0 ];
+        let testRate = undefined;
+
+        let result = bo.changeTaxes( testObj , testRate );
+        if ( result )
+        {
+            assert.fail( "changeTaxes returned for undefined newRate: " + result );
+        }
+
+        done();
+    });
+
+    it( 'test changeTaxes failure for undefined old rate' , function( done )
+    {
+        let mockDataList = JSON.parse(JSON.stringify( require( "../sampleData/testData/planetsDifferentSystems.json" ) ) );
+        assert.ok( mockDataList );
+        assert.ok( mockDataList.length );
+        assert.ok( mockDataList[ 0 ] );
+
+        let testObj = mockDataList[ 0 ];
+        let testRate = 0.1;
+        assert.ok( testRate <= MAX_TAX_RATE );
+        mockDataList[ 0 ].taxes = undefined;
+
+        let result = bo.changeTaxes( testObj , testRate );
+        if ( result )
+        {
+            assert.fail( "changeTaxes returned for undefined old rate: " + result );
+        }
+
+        done();
+    });
+
+    /*
+    ===========
+    _____ testing begins
+    ===========
+    */
+
+    //after() is run after all tests have completed.
+    after( function( done )
+    {
+        done();
+    });
+});
